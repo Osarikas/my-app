7a4cc0d10d19648c8c2f51f29245cbeb15a232c1
Osarikas <138009295+Osarikas@users.noreply.github.com>
2024-09-06 19:34:14 +0300

    Ы

---
+++ src/components/Product.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { IProduct } from '../models'
+interface ProductProps {
+  product: IProduct
+}
+export default function Product(props: ProductProps) {
+  return (
+    <div className="border py-2 px-4 rounded flex flex-col items-center mb-2">
+      {props.product.name}
+    </div>
+  )
+}
---
+++ src/models.ts
@@ -0,0 +1,25 @@
+export interface IProduct {
+  product_id: number
+  name: string
+  description: string
+  price: number
+  unit: string
+  image: string
+  discount: number
+  availability: boolean
+  brand: string
+  category: string
+  rating: number
+  reviews: [
+    {
+      user_id: number
+      rating: number
+      comment: string
+    },
+    {
+      user_id: number
+      rating: number
+      comment: string
+    }
+  ]
+}
